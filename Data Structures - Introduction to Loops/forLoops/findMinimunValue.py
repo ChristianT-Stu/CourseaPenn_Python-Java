60d783d6d0ee13ea95c2afbd3210799f7aaed7f3
ChristianT-Stu <69771935+ChristianT-Stu@users.noreply.github.com>
2023-05-03 14:45:28 -0400

    Initial commit

---
+++ Data Structures - Introduction to Loops/forLoops/findMinimunValue.py
@@ -0,0 +1,20 @@
+# Write code that find the minimum value of a list of numbers (5,3,8,-1,-2.2,0)
+'''
+    - Don't use the built in min() function
+    - Instantiate a 'numbers' list variable containing the proper values (above)
+    - Iterate over the list and find the min value
+    - Print the minimum value in the format: "...is the min number"
+'''
+
+#define list of numbers
+numbers = [5,3,8,-1,-2.2,0]
+
+#define min number variable
+min_number = numbers[0]
+
+#iterate over this list for the min value
+for number in numbers:
+    if number < min_number:
+        min_number = number
+
+print(min_number, "is the min number.")
